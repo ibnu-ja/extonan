93a0b12be1a271232dd6d9ad0a3c168fb63c4506
Ibnu <ibnu.magang@gmail.com>
2021-10-12 14:44:53 +0700

    dashboard layout teams

--- resources/js/Layouts/Dash/List.vue
+++ resources/js/Layouts/Dash/List.vue
@@ -31,6 +31,59 @@
         :item="item"
       />
     </template>
+    <!-- Team Management -->
+    <template v-if="$page.props.jetstream.hasTeamFeatures">
+      <v-subheader v-if="mini">
+        MT
+      </v-subheader>
+      <v-subheader v-else>
+        Manage Team
+      </v-subheader>
+
+      <inertia-link
+        as="v-list-item"
+        :href="route('teams.show', $page.props.user.current_team)"
+        :active="route().current('teams.show')"
+      >
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Team Settings</v-list-item-title>
+      </inertia-link>
+      <inertia-link
+        as="v-list-item"
+        :href="route('teams.create')"
+        :active="route().current('teams.create')"
+      >
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple-plus-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Create New Team</v-list-item-title>
+      </inertia-link>
+
+      <v-subheader v-if="mini">
+        ST
+      </v-subheader>
+      <v-subheader v-else>
+        Switch Teams
+      </v-subheader>
+
+      <template v-for="team in $page.props.user.all_teams">
+        <v-list-item
+          :key="team.id"
+          @click.prevent="switchToTeam(team)"
+        >
+          <!-- <svg v-if="team.id == $page.props.user.current_team_id" class="mr-l" style="width: 1.25rem; height: 1.25rem; color: rgba(52, 211, 153, 1); " fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> -->
+          <v-list-item-icon v-if="team.id == $page.props.user.current_team_id">
+            <v-icon style="color: rgba(52, 211, 153, 1); ">
+              mdi-check-circle-outline
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ team.name }}</v-list-item-title>
+        </v-list-item>
+      </template>
+      <v-divider />
+    </template>
     <v-divider />
     <slot name="extra" />
   </v-list>
