9b84486944b53bc737bbdb11c4f1dcc3f2a16b98
Ibnu <ibnu.magang@gmail.com>
2021-10-12 14:45:53 +0700

    testi homepage

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,93 +1,44 @@
 <template>
   <web-layout>
-    <v-container>
-      <v-row
-        justify="center"
-        class="py-4 mb-10 pt-0"
-        align="center"
+    <v-row>
+      <v-col
+        cols="12"
+        md="9"
+        order-md="2"
       >
-        <v-col
-          cols="12"
-          md="10"
-          order="first"
-          class="align-center pr-6 "
-        >
-          <h1 class="text-h4 text-sm-h3 text-lg-h2 mt-xl-4 font-weight-black">
-            Material design in Jetstream with
-            <span class="primary--text">JetVuetify</span>
-          </h1>
-          <h2 class="text-h6 text-sm-h5 secondary--text mt-4 pr-6">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
-            hic blanditiis enim! Delectus eius facere cum incidunt officiis?
-          </h2>
-          <v-btn
-            x-large
-            color="secondary"
-            class="mt-6"
-          >
-            Documentation
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-sheet>
-      <v-container
-        fluid
-        class="py-4 py-lg-8"
+        <v-card :tile="$vuetify.breakpoint.smAndDown">
+          <v-card-title>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </v-card-title>
+          <v-container class="px-4 pb-4 pt-0">
+            aaa
+          </v-container>
+          <v-card-actions><v-btn>a</v-btn></v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="3"
+        order-md="1"
       >
-        <v-row>
-          <v-col
-            cols="12"
-            md="6"
-            class="my-auto"
-          >
-            <SvgBusinessAnalytics style="height: 300px;" />
-          </v-col>
-          <v-col
-            cols="12"
-            md="6"
-            class="my-auto"
-          >
-            <div class="my-10 pa-2 pt-5 rounded">
-              <div class="title">
-                Feature title
-              </div>
-              <div class="text-body-1">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
-                laudantium dolore, quasi similique eveniet id cupiditate unde,
-                qui cumque fugit repudiandae ipsa recusandae delectus, quidem
-                enim suscipit commodi vitae modi.
-              </div>
-            </div>
-            <div class="pa-2 rounded">
-              <div class="title">
-                Feature title
-              </div>
-              <div class="text-body-1">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-                ratione tenetur, alias sunt porro, incidunt, atque perspiciatis
-                suscipit distinctio commodi maxime beatae tempore. Quos
-                perspiciatis rem accusamus ad, ipsum veritatis?
-              </div>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-sheet>
+        <v-card :tile="$vuetify.breakpoint.smAndDown">
+          <v-card-title>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </v-card-title>
+          <v-container class="px-4 pb-4 pt-0">
+            aaa
+          </v-container>
+          <v-card-actions><v-btn>a</v-btn></v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </web-layout>
 </template>
 
 <script>
-import WebLayout from '@/Layouts/WebLayout.vue'
-// import ValidationErrors from '@/Components/ValidationErrors'
-import SvgBusinessAnalytics from '@/Components/svg/BusinessAnalytics'
-
+import WebLayout from '@/Layouts/Web/Index.vue'
 export default {
-  components: {
-    WebLayout,
-    // ValidationErrors,
-    SvgBusinessAnalytics
-  },
+  components: { WebLayout },
   data () {
     return {
       signUpEmail: false,
@@ -101,6 +52,10 @@ export default {
         terms: true
       })
     }
+  },
+
+  created () {
+    console.log(this.$vuetify.breakpoint.smAndDown)
   },
 
   methods: {
