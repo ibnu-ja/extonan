8b0db0a2e04633ee9cf471ba86d9defb6bd604a6
Ibnu <ibnu.magang@gmail.com>
2021-10-12 14:50:06 +0700

    vuex state for dash layout

---
+++ resources/js/store/Modules/dashboard.js
@@ -0,0 +1,24 @@
+// Utilities
+import { make } from 'vuex-pathify'
+
+const state = {
+  mini: false,
+  drawer: null
+}
+// aaa
+
+const mutations = make.mutations(state)
+
+const actions = {
+  ...make.actions(state)
+}
+
+const getters = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
