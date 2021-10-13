37da30d6685ea75e0bf2430d6e2caeec7737e031
Ibnu <ibnu.magang@gmail.com>
2021-10-14 00:00:46 +0700

    indexing and edit button

--- resources/js/Pages/Dashboard/Album/Index.vue
+++ resources/js/Pages/Dashboard/Album/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <dash-layout>
+    <confirm-dialog ref="confirm" />
     <template #header>
       <h3>Album</h3>
     </template>
@@ -14,13 +15,36 @@
             Tambah
           </inertia-link>
         </v-toolbar>
-        <v-data-table />
+        <v-data-table
+          :items="album"
+          :headers="headers"
+        >
+          <template #[`item.actions`]="{ item }">
+            <v-btn
+              small
+              icon
+              color="info"
+              :to="'/dashboard/album/' + item.id + '/edit'"
+            >
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn
+              small
+              icon
+              color="error"
+              @click="destroy(item.id)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
       </v-card>
     </div>
   </dash-layout>
 </template>
 
 <script>
+import ConfirmDialog from '@/Components/ConfirmDialog.vue'
 import DashLayout from '@/Layouts/Dash/Index.vue'
 import { VBtn } from 'vuetify/lib'
 
@@ -28,9 +52,37 @@ export default {
   name: 'DashboardAlbum',
   components: {
     DashLayout,
+    ConfirmDialog,
     // eslint-disable-next-line vue/no-unused-components
     VBtn
   },
-  data: () => ({ dialog: false, tanggal: false, date: '' })
+  props: {
+    album: {
+      type: Array,
+      default: null
+    }
+  },
+  data: () => ({
+    dialog: false,
+    tanggal: false,
+    date: '',
+    headers: [
+      {
+        text: 'ID',
+        value: 'id'
+      },
+      { text: 'Name', value: 'name' },
+      { text: 'Date', value: 'updated_at' },
+      { text: 'Actions', value: 'actions' }
+    ]
+  }),
+  methods: {
+    destroy (id) {
+      // confirm
+      this.$refs.confirm
+        .open('Delete', 'Are you sure to delete this item?', { color: 'red' })
+        .then(() => this.$inertia.delete('/dashboard/album/' + id))
+    }
+  }
 }
 </script>
