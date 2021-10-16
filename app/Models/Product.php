c5e66287cf2282d12d98d593ef69fdc1e98e666b
Ibnu <ibnu.magang@gmail.com>
2021-10-17 04:07:06 +0700

    product model mass assignment & slug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'name',
+        'name_real',
+        'release_date',
+        'type',
+        'desc',
+        'meta',
+    ];
+
+    protected $casts = [
+        'meta' => 'json',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
