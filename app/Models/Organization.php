a39ded5f206be232072384a77e69b77754cd3f4e
Ibnu JA <ibnu.magang@gmail.com>
2021-10-21 21:17:40 +0700

    organization fillable and slug

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Organization extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'name',
+        'name_real',
+        'desc',
+        'region',
+        'type',
+        'meta',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
