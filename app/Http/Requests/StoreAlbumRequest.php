847588d4dffe646d2f1fc40d927aed4824aea286
Ibnu JA <ibnu.magang@gmail.com>
2021-11-06 01:01:52 +0700

    album validation orgs

--- app/Http/Requests/StoreAlbumRequest.php
+++ app/Http/Requests/StoreAlbumRequest.php
@@ -36,6 +36,7 @@ class StoreAlbumRequest extends FormRequest
             'media_format' => 'string',
             'desc' => 'string',
             'roles' => 'nullable|array|min:1',
+            'orgs' => 'nullable|array|min:1',
             'roles.composers' => 'nullable|array',
             'roles.performers' => 'nullable|array',
             'roles.lyricists' => 'nullable|array',
@@ -44,6 +45,14 @@ class StoreAlbumRequest extends FormRequest
             'roles.performers.*' => 'sometimes|numeric|exists:artists,id',
             'roles.lyricists.*' => 'sometimes|numeric|exists:artists,id',
             'roles.arrangers.*' => 'sometimes|numeric|exists:artists,id',
+            'orgs.label' => 'nullable|array',
+            'orgs.publisher' => 'nullable|array',
+            'orgs.distributor' => 'nullable|array',
+            'orgs.manufacturer' => 'nullable|array',
+            'orgs.composers.*' => 'sometimes|numeric|exists:organizations,id',
+            'orgs.performers.*' => 'sometimes|numeric|exists:organizations,id',
+            'orgs.lyricists.*' => 'sometimes|numeric|exists:organizations,id',
+            'orgs.arrangers.*' => 'sometimes|numeric|exists:organizations,id',
         ];
     }
 }
