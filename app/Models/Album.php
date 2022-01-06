d75fb79dc7d567c002ca5b41b40e133914d1e456
Ibnu JA <ibnu.magang@gmail.com>
2022-01-06 15:22:30 +0700

    preparing Model for media upload

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Album extends Model
+class Album extends Model implements HasMedia
 //  implements HasMedia
 {
     use HasSlug;
-    // use InteractsWithMedia;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'name',
