d0435c31a767477a4601e13dc13e748bd05f4196
Ibnu JA <ibnu.magang@gmail.com>
2022-01-07 14:51:25 +0700

    Latest Album placeholder

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,34 +2,118 @@
   <web-layout>
     <v-row>
       <v-col
+        :class="{'px-0' : $vuetify.breakpoint.smAndDown}"
         cols="12"
         md="9"
         order-md="2"
       >
-        <v-card :tile="$vuetify.breakpoint.smAndDown">
-          <v-card-title>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </v-card-title>
-          <v-container class="px-4 pb-4 pt-0">
-            aaa
-          </v-container>
-          <v-card-actions><v-btn>a</v-btn></v-card-actions>
-        </v-card>
+        <v-row>
+          <v-col cols="12">
+            <v-card :tile="$vuetify.breakpoint.smAndDown">
+              <v-toolbar
+                flat
+                dense
+              >
+                <v-toolbar-title><h4>Latest Album</h4></v-toolbar-title>
+                <v-spacer />
+                <v-btn small>
+                  view all
+                </v-btn>
+              </v-toolbar>
+              <v-card-text>
+                <v-row dense>
+                  <v-col
+                    v-for="card in cards"
+                    :key="card.title"
+                    :md="card.flex"
+                    cols="6"
+                  >
+                    <v-card :to="'/dashboard'">
+                      <v-img
+                        :src="card.src"
+                        class="white--text align-end"
+                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                        height="200px"
+                      >
+                        <v-card-title v-text="card.title" />
+                      </v-img>
+
+                      <v-card-actions>
+                        <v-list-item
+                          class="grow"
+                          dense
+                        >
+                          <v-row
+                            align="center"
+                            justify="start"
+                          >
+                            <v-icon class="mr-1">
+                              mdi-calendar
+                            </v-icon>
+                            <span class="subheading">24 Jan 2022</span>
+                            <v-spacer />
+                            <v-icon class="mr-1">
+                              mdi-heart
+                            </v-icon>
+                            <span class="subheading">256</span>
+                          </v-row>
+                        </v-list-item>
+                      </v-card-actions>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col
+        :class="{'px-0' : $vuetify.breakpoint.smAndDown}"
         cols="12"
         md="3"
         order-md="1"
       >
-        <v-card :tile="$vuetify.breakpoint.smAndDown">
-          <v-card-title>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </v-card-title>
-          <v-container class="px-4 pb-4 pt-0">
-            aaa
-          </v-container>
-          <v-card-actions><v-btn>a</v-btn></v-card-actions>
-        </v-card>
+        <v-row>
+          <v-col cols="12">
+            <v-card :tile="$vuetify.breakpoint.smAndDown">
+              <v-toolbar
+                flat
+                dense
+              >
+                <v-toolbar-title><h4>Popular Artists</h4></v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                aaa
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card :tile="$vuetify.breakpoint.smAndDown">
+              <v-toolbar
+                flat
+                dense
+              >
+                <v-toolbar-title><h4>Newest Productssss</h4></v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                aaa
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card :tile="$vuetify.breakpoint.smAndDown">
+              <v-toolbar
+                flat
+                dense
+              >
+                <v-toolbar-title><h4>Newest Orgs</h4></v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                aaa
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </web-layout>
@@ -39,32 +123,12 @@
 import WebLayout from '@/Layouts/Web/Index.vue'
 export default {
   components: { WebLayout },
-  data () {
-    return {
-      signUpEmail: false,
-      showP: false,
-      showCP: false,
-      form: this.$inertia.form({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-        terms: true
-      })
-    }
-  },
-
-  created () {
-    console.log(this.$vuetify.breakpoint.smAndDown)
-  },
-
-  methods: {
-    submit () {
-      this.form.password_confirmation = this.form.password
-      this.form.post(this.route('register'), {
-        onFinish: () => this.form.reset('password', 'password_confirmation')
-      })
-    }
-  }
+  data: () => ({
+    cards: [
+      { title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', src: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', flex: 4 },
+      { title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', src: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', flex: 4 },
+      { title: 'Is the order a rabbit? 10th Anniversary Character Song', src: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', flex: 4 }
+    ]
+  })
 }
 </script>
