135c2504c7346733b7aea6ba3d96ba9a62dfcb67
Ibnu JA <ibnu.magang@gmail.com>
2022-01-07 16:02:57 +0700

    artist section

--- resources/js/Pages/HomeSections/PopularArtist.vue
+++ resources/js/Pages/HomeSections/PopularArtist.vue
@@ -1,19 +1,65 @@
 <template>
-  <v-card :tile="$vuetify.breakpoint.smAndDown">
-    <v-toolbar
-      flat
-      dense
-    >
-      <v-toolbar-title><h4>Popular Artists</h4></v-toolbar-title>
-    </v-toolbar>
-    <v-card-text>
-      aaa
-    </v-card-text>
-  </v-card>
+  <section id="popular-artists">
+    <v-card>
+      <v-img
+        style="text-decoration: none;"
+        height="180px"
+        class="white--text align-end"
+        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+        :src="artists[0].image"
+      >
+        <inertia-link
+          href="/dashboard"
+          style="text-decoration: none;"
+        >
+          <v-app-bar
+            flat
+            dense
+            color="rgba(0, 0, 0, 0)"
+          >
+            <v-toolbar-title class="text-h6 white--text pl-0">
+              Popular Artists
+            </v-toolbar-title>
+          </v-app-bar>
+
+          <v-card-title class="white--text mt-8">
+            <p
+              class="ml-3"
+              v-text="artists[0].name"
+            />
+          </v-card-title>
+        </inertia-link>
+      </v-img>
+      <v-list-item
+        v-for="(artist) in unpopular"
+        :key="artist.name"
+        to="/dashboard"
+      >
+        <v-list-item-avatar>
+          <v-img
+            :alt="`${artist.name} avatar`"
+            :src="artist.image"
+          />
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="artist.name" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </section>
 </template>
 
 <script>
 export default {
-
+  name: 'PopularArtists',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['artists'],
+  computed: {
+    unpopular () {
+      // shift first el
+      return this.artists.slice(1, this.artists.length)
+    }
+  }
 }
 </script>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,7 +17,7 @@
       >
         <v-row>
           <v-col cols="12">
-            <popular-artist />
+            <popular-artist :artists="artists" />
           </v-col>
           <v-col cols="12">
             <v-card :tile="$vuetify.breakpoint.smAndDown">
@@ -65,6 +65,28 @@ export default {
       { id: 3, title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
       { id: 4, title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
       { id: 5, title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' }
+    ],
+    artists: [
+      {
+        image: 'https://www.themoviedb.org/t/p/w235_and_h235_face/sSXB0DOmfLy1BGsTN7q98Q9yzAf.jpg',
+        name: 'Minase Inori'
+      },
+      {
+        image: 'https://media.vgm.io/artists/98/13289/13289-1591844020.jpg',
+        name: 'Uchida Maaya'
+      },
+      {
+        image: 'https://cdn.myanimelist.net/images/voiceactors/3/60503.jpg',
+        name: 'Kayano Ai'
+      },
+      {
+        image: 'https://cdn.myanimelist.net/images/voiceactors/3/62976.jpg',
+        name: 'Taketatsu Ayana'
+      },
+      {
+        image: 'https://cdn.myanimelist.net/images/voiceactors/2/54667.jpg',
+        name: 'Ayane Sakura'
+      }
     ]
   })
 }
