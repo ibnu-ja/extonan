c1ca1993274c27536d46d77c67c55347fc161933
Ibnu JA <ibnu.magang@gmail.com>
2022-01-07 15:18:32 +0700

    moving latest album to section

---
+++ resources/js/Pages/HomeSections/LatestAlbum.vue
@@ -0,0 +1,81 @@
+<template>
+  <section id="latest-album">
+    <v-row>
+      <v-col cols="12">
+        <v-card :tile="$vuetify.breakpoint.smAndDown">
+          <v-toolbar
+            flat
+            dense
+          >
+            <v-toolbar-title><h4>Latest Album</h4></v-toolbar-title>
+            <v-spacer />
+            <v-btn small>
+              view all
+            </v-btn>
+          </v-toolbar>
+          <v-card-text>
+            <v-row dense>
+              <v-col
+                v-for="album in albums"
+                :key="album.id"
+                md="4"
+                cols="6"
+              >
+                <v-card :to="'/album/'+album.slug">
+                  <v-img
+                    :src="album.cover"
+                    class="white--text align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="200px"
+                  >
+                    <v-card-title v-text="album.title" />
+                  </v-img>
+
+                  <v-card-actions>
+                    <v-list-item
+                      class="grow"
+                      dense
+                    >
+                      <v-row
+                        align="center"
+                        justify="start"
+                      >
+                        <v-icon class="mr-1">
+                          mdi-calendar
+                        </v-icon>
+                        <span class="subheading">{{ album.date_created }}</span>
+                        <v-spacer />
+                        <v-icon class="mr-1">
+                          mdi-heart
+                        </v-icon>
+                        <span class="subheading">{{ album.heart }}</span>
+                      </v-row>
+                    </v-list-item>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LatestAlbum',
+  props: {
+    albums: {
+      type: Array,
+      default: () => [{
+        id: 0,
+        title: 'N/A',
+        cover: '',
+        heart: 0,
+        date_created: 'N/A'
+      }]
+    }
+  }
+}
+</script>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -7,65 +7,7 @@
         md="9"
         order-md="2"
       >
-        <v-row>
-          <v-col cols="12">
-            <v-card :tile="$vuetify.breakpoint.smAndDown">
-              <v-toolbar
-                flat
-                dense
-              >
-                <v-toolbar-title><h4>Latest Album</h4></v-toolbar-title>
-                <v-spacer />
-                <v-btn small>
-                  view all
-                </v-btn>
-              </v-toolbar>
-              <v-card-text>
-                <v-row dense>
-                  <v-col
-                    v-for="card in cards"
-                    :key="card.title"
-                    :md="card.flex"
-                    cols="6"
-                  >
-                    <v-card :to="'/dashboard'">
-                      <v-img
-                        :src="card.src"
-                        class="white--text align-end"
-                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                        height="200px"
-                      >
-                        <v-card-title v-text="card.title" />
-                      </v-img>
-
-                      <v-card-actions>
-                        <v-list-item
-                          class="grow"
-                          dense
-                        >
-                          <v-row
-                            align="center"
-                            justify="start"
-                          >
-                            <v-icon class="mr-1">
-                              mdi-calendar
-                            </v-icon>
-                            <span class="subheading">24 Jan 2022</span>
-                            <v-spacer />
-                            <v-icon class="mr-1">
-                              mdi-heart
-                            </v-icon>
-                            <span class="subheading">256</span>
-                          </v-row>
-                        </v-list-item>
-                      </v-card-actions>
-                    </v-card>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
+        <latest-album :albums="albums" />
       </v-col>
       <v-col
         :class="{'px-0' : $vuetify.breakpoint.smAndDown}"
@@ -121,13 +63,17 @@
 
 <script>
 import WebLayout from '@/Layouts/Web/Index.vue'
+import LatestAlbum from './HomeSections/LatestAlbum.vue'
 export default {
-  components: { WebLayout },
+  components: { WebLayout, LatestAlbum },
   data: () => ({
-    cards: [
-      { title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', src: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', flex: 4 },
-      { title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', src: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', flex: 4 },
-      { title: 'Is the order a rabbit? 10th Anniversary Character Song', src: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', flex: 4 }
+    albums: [
+      { title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
+      { title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
+      { title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' },
+      { title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
+      { title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
+      { title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' }
     ]
   })
 }
