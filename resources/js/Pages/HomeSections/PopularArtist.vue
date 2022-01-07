ee09269677c2f13bbadec3cc578b0eca175160c7
Ibnu JA <ibnu.magang@gmail.com>
2022-01-07 15:22:48 +0700

    give id to album placeholder, artist section

---
+++ resources/js/Pages/HomeSections/PopularArtist.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card :tile="$vuetify.breakpoint.smAndDown">
+    <v-toolbar
+      flat
+      dense
+    >
+      <v-toolbar-title><h4>Popular Artists</h4></v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      aaa
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,17 +17,7 @@
       >
         <v-row>
           <v-col cols="12">
-            <v-card :tile="$vuetify.breakpoint.smAndDown">
-              <v-toolbar
-                flat
-                dense
-              >
-                <v-toolbar-title><h4>Popular Artists</h4></v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                aaa
-              </v-card-text>
-            </v-card>
+            <popular-artist />
           </v-col>
           <v-col cols="12">
             <v-card :tile="$vuetify.breakpoint.smAndDown">
@@ -64,16 +54,17 @@
 <script>
 import WebLayout from '@/Layouts/Web/Index.vue'
 import LatestAlbum from './HomeSections/LatestAlbum.vue'
+import PopularArtist from './HomeSections/PopularArtist.vue'
 export default {
-  components: { WebLayout, LatestAlbum },
+  components: { WebLayout, LatestAlbum, PopularArtist },
   data: () => ({
     albums: [
-      { title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
-      { title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
-      { title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' },
-      { title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
-      { title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
-      { title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' }
+      { id: 0, title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
+      { id: 1, title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
+      { id: 2, title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' },
+      { id: 3, title: 'Is the order a rabbit? BALLADE SONG ALBUM: Blend of Letters', cover: 'https://media.vgm.io/albums/51/100315/100315-e7c38585b311.jpg', heart: 25, date_created: '30 Dec 2020' },
+      { id: 4, title: 'Is the order a rabbit? Main Theme Rearrange & High Resolution Collection', cover: 'https://media.vgm.io/albums/05/112150/112150-6d73daf0c83b.jpg', heart: 123, date_created: '30 Dec 2020' },
+      { id: 5, title: 'Is the order a rabbit? 10th Anniversary Character Song', cover: 'https://media.vgm.io/albums/94/112149/112149-dda2d5ab9fb8.jpg', heart: 30, date_created: '25 Jan 2022' }
     ]
   })
 }
