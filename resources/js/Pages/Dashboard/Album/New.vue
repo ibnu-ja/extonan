83b761015b0710dfe24ada7e4e7b49bfc39ebe7d
Ibnu JA <ibnu.magang@gmail.com>
2022-01-14 04:25:46 +0700

    move data album init to each page

--- resources/js/Pages/Dashboard/Album/New.vue
+++ resources/js/Pages/Dashboard/Album/New.vue
@@ -5,6 +5,47 @@ import Forms from './Forms.vue'
 export default {
   name: 'DashboardAlbum',
   extends: Forms,
+  data () {
+    return {
+      album: this.$inertia.form({
+        images: null,
+        imageLabel: [],
+        name: '',
+        name_real: '',
+        name_trans: '',
+        event_id: '',
+        barcode: '',
+        catalog: '',
+        release_date: '',
+        desc: '',
+        roles: {
+          arrangers: [],
+          performers: [],
+          lyricists: [],
+          composers: []
+        },
+        orgs: {
+          label: [],
+          publisher: [],
+          distributor: [],
+          manufacturer: []
+        },
+        discs: [
+          {
+            disc_length: '',
+            name: '',
+            tracks: [
+              {
+                names: {},
+                track_length: ''
+              }
+            ]
+          }
+        ],
+        media_format: ''
+      })
+    }
+  },
   methods: {
     submit () {
       this.album.post(this.route('album.store'))
