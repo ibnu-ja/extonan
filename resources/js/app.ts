c7e10c72b6f38c3c2b94bcc0194bb23251695383
Ibnu JA <ibnu.ja@outlook.com>
2024-06-30 08:14:10 +0000

    feat: install vuetify

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,5 +1,5 @@
-import './bootstrap'
-import '../css/app.css'
+import './bootstrap';
+import '../css/app.scss';
 
 import { createApp, h, DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
@@ -10,18 +10,20 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 import { Config } from 'ziggy-js'
 
 import * as Routes from './routes.json'
+import Vuetify from "@/plugins/vuetify";
 const config = Routes as Config
 
 createInertiaApp({
-  title: title => `${title} - ${appName}`,
-  resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(ZiggyVue, config)
-      .mount(el)
-  },
-  progress: {
-    color: '#4B5563',
-  },
-}).then()
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, config)
+            .use(Vuetify)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+}).then();
---
+++ resources/js/plugins/vuetify-ssr.ts
@@ -0,0 +1,22 @@
+// Styles
+import '../../css/app.scss';
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+// import { md3 } from 'vuetify/blueprints'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
+const vuetify = createVuetify({
+    // blueprint: md3,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    ssr: true
+})
+
+export default vuetify
