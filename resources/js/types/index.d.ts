ee30d11a07c72d22f9fd63387fcef335d06bd711
Ibnu JA <ibnu.ja@outlook.com>
2024-07-03 14:55:15 +0000

    feat: add type

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,11 +5,24 @@ export interface User {
   name: string
   email: string
   email_verified_at: string
+  profile_photo_url?: string
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
   auth: {
     user: User
+  }
+  jetstream?: {
+    canCreateTeams?: boolean
+    canManageTwoFactorAuthentication?: boolean
+    canUpdatePassword?: boolean
+    canUpdateProfileInformation?: boolean
+    flash: { [key: string]: string | string[] | unknown }
+    hasAccountDeletionFeatures?: boolean
+    hasApiFeatures?: boolean
+    hasTeamFeatures?: boolean
+    hasTermsAndPrivacyPolicyFeature?: boolean
+    managesProfilePhotos?: boolean
   }
   ziggy: Config & { location: string }
 }
