4ad1d566ee1113e2b28a01c3fe4020ea44c49b82
Ibnu JA <ibnu.ja@outlook.com>
2024-07-03 15:18:38 +0000

    feat: home page template

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+import { onBeforeMount } from 'vue'
+import { useTheme } from 'vuetify'
+import AppBar from '@/Layouts/Partials/AppBar.vue'
+import Drawer from '@/Layouts/Partials/Drawer.vue'
+
+const vuetifyTheme = useTheme()
+
+onBeforeMount(() => {
+  vuetifyTheme.global.name.value = 'dark'
+})
+
+</script>
+<template>
+  <v-layout>
+    <AppBar />
+    <Drawer />
+    <v-main>
+      <slot />
+    </v-main>
+  </v-layout>
+</template>
---
+++ resources/js/Layouts/Partials/Drawer.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const drawer = ref<boolean | null>(null)
+</script>
+
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    temporary
+    location="top"
+  >
+    <v-list>
+      <v-list-item
+        title="My Application"
+        subtitle="Vuetify"
+      />
+      <v-divider />
+      <v-list-item
+        link
+        title="List Item 1"
+      />
+      <v-list-item
+        link
+        title="List Item 2"
+      />
+      <v-list-item
+        link
+        title="List Item 3"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
