5e1b9157632f8b6c7e653c608be0d7f260310921
Ibnu JA <ibnu.ja@outlook.com>
2024-07-09 14:52:16 +0000

    chore: remove unused

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -157,57 +157,5 @@ const submit = () => {
         />
       </v-card-text>
     </v-card>
-    <!--      <AuthenticationCard>-->
-    <!--          <template #logo>-->
-    <!--              <AuthenticationCardLogo/>-->
-    <!--          </template>-->
-
-    <!--          <form @submit.prevent="submit">-->
-    <!--              <div>-->
-    <!--                  <InputLabel for="email" value="Email"/>-->
-    <!--                  <TextInput-->
-    <!--                      id="email"-->
-    <!--                      v-model="form.email"-->
-    <!--                      autofocus-->
-    <!--                      class="mt-1 block w-full"-->
-    <!--                      required-->
-    <!--                      type="email"-->
-    <!--                  />-->
-    <!--                  <InputError :message="form.errors.email" class="mt-2"/>-->
-    <!--              </div>-->
-
-    <!--              <div class="mt-4">-->
-    <!--                  <InputLabel for="password" value="Password"/>-->
-    <!--                  <TextInput-->
-    <!--                      id="password"-->
-    <!--                      v-model="form.password"-->
-    <!--                      autocomplete="current-password"-->
-    <!--                      class="mt-1 block w-full"-->
-    <!--                      required-->
-    <!--                      type="password"-->
-    <!--                  />-->
-    <!--                  <InputError :message="form.errors.password" class="mt-2"/>-->
-    <!--              </div>-->
-
-    <!--              <div class="block mt-4">-->
-    <!--                  <label class="flex items-center">-->
-    <!--                      <Checkbox v-model:checked="form.remember" name="remember"/>-->
-    <!--                      <span class="ml-2 text-sm text-gray-600">Remember me</span>-->
-    <!--                  </label>-->
-    <!--              </div>-->
-
-    <!--              <div class="flex items-center justify-end mt-4">-->
-    <!--                  <Link v-if="canResetPassword" :href="route('password.request')"-->
-    <!--                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">-->
-    <!--                      Forgot your password?-->
-    <!--                  </Link>-->
-
-    <!--                  <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" class="ml-4">-->
-    <!--                      Log in-->
-    <!--                  </PrimaryButton>-->
-    <!--              </div>-->
-    <!--          </form>-->
-
-    <!--      </AuthenticationCard>-->
   </AuthLayout>
 </template>
