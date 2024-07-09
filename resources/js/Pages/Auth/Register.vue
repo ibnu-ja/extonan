57c2753e93d8a1a280dcf5dff4099007c504cb72
Ibnu JA <ibnu.ja@outlook.com>
2024-07-09 14:52:28 +0000

    feat: login page

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,13 +1,12 @@
-<script setup>
-import { Head, Link, useForm } from '@inertiajs/vue3'
-import AuthenticationCard from '@/Components/AuthenticationCard.vue'
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue'
-import Checkbox from '@/Components/Checkbox.vue'
-import InputError from '@/Components/InputError.vue'
-import InputLabel from '@/Components/InputLabel.vue'
-import PrimaryButton from '@/Components/PrimaryButton.vue'
+<script setup lang="ts">
+import { Head, useForm, usePage } from '@inertiajs/vue3'
+import AuthLayout from '@/Layouts/AuthLayout.vue'
+import { VBtn } from 'vuetify/components'
+import InertiaLink from '@/Components/InertiaLink.vue'
+import { ref } from 'vue'
+import { mdiEye } from '@mdi/js/commonjs/mdi'
+import { mdiEyeOff } from '@mdi/js'
 import Socialstream from '@/Components/Socialstream.vue'
-import TextInput from '@/Components/TextInput.vue'
 
 const form = useForm({
   name: '',
@@ -17,157 +16,166 @@ const form = useForm({
   terms: false,
 })
 
+const showP = ref(false)
+const showCP = ref(false)
+
+const test = () => alert('suuu')
+
 const submit = () => {
+  console.log('submitting')
   form.post(route('register'), {
     onFinish: () => form.reset('password', 'password_confirmation'),
   })
 }
+
+const inertiaProps = usePage().props
+
 </script>
 
 <template>
   <Head title="Register" />
-
-  <AuthenticationCard>
-    <template #logo>
-      <AuthenticationCardLogo />
-    </template>
-
-    <form @submit.prevent="submit">
-      <div>
-        <InputLabel
-          for="name"
-          value="Name"
-        />
-        <TextInput
-          id="name"
-          v-model="form.name"
-          autocomplete="name"
-          autofocus
-          class="mt-1 block w-full"
-          required
-          type="text"
-        />
-        <InputError
-          :message="form.errors.name"
-          class="mt-2"
-        />
-      </div>
-
-      <div class="mt-4">
-        <InputLabel
-          for="email"
-          value="Email"
-        />
-        <TextInput
-          id="email"
-          v-model="form.email"
-          autocomplete="username"
-          class="mt-1 block w-full"
-          required
-          type="email"
-        />
-        <InputError
-          :message="form.errors.email"
-          class="mt-2"
-        />
-      </div>
-
-      <div class="mt-4">
-        <InputLabel
-          for="password"
-          value="Password"
-        />
-        <TextInput
-          id="password"
-          v-model="form.password"
-          autocomplete="new-password"
-          class="mt-1 block w-full"
-          required
-          type="password"
-        />
-        <InputError
-          :message="form.errors.password"
-          class="mt-2"
-        />
-      </div>
-
-      <div class="mt-4">
-        <InputLabel
-          for="password_confirmation"
-          value="Confirm Password"
-        />
-        <TextInput
-          id="password_confirmation"
-          v-model="form.password_confirmation"
-          autocomplete="new-password"
-          class="mt-1 block w-full"
-          required
-          type="password"
-        />
-        <InputError
-          :message="form.errors.password_confirmation"
-          class="mt-2"
-        />
-      </div>
-
-      <div
-        v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature"
-        class="mt-4"
-      >
-        <InputLabel for="terms">
-          <div class="flex items-center">
-            <Checkbox
-              id="terms"
-              v-model:checked="form.terms"
-              name="terms"
-              required
-            />
-
-            <div class="ml-2">
-              I agree to the <a
-                :href="route('terms.show')"
-                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                target="_blank"
-              >Terms
-                of Service</a> and <a
-                :href="route('policy.show')"
-                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                target="_blank"
-              >Privacy
-                Policy</a>
-            </div>
-          </div>
-          <InputError
-            :message="form.errors.terms"
-            class="mt-2"
-          />
-        </InputLabel>
-      </div>
-
-      <div class="flex items-center justify-end mt-4">
-        <Link
-          :href="route('login')"
-          class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Already registered?
-        </Link>
-
-        <PrimaryButton
-          :class="{ 'opacity-25': form.processing }"
+  <AuthLayout>
+    <InertiaLink
+      :as="VBtn"
+      href="/"
+      flat
+      icon
+      size="100"
+      class="mb-4"
+    >
+      <v-avatar
+        color="grey darken-1"
+        size="100"
+      />
+    </InertiaLink>
+    <v-btn @click="test">
+      asd
+    </v-btn>
+    <v-card
+      width="100%"
+      max-width="400px"
+    >
+      <v-card-text>
+        <v-form
+          class="mx-auto"
           :disabled="form.processing"
-          class="ml-4"
+          @submit.prevent="submit"
         >
-          Register
-        </PrimaryButton>
-      </div>
-    </form>
+          <h2 class="text-h4 mb-4">
+            Welcome!
+          </h2>
 
-    <Socialstream
-      v-if="$page.props.socialstream.show && $page.props.socialstream.providers.length"
-      :error="$page.props?.errors?.socialstream || null"
-      :prompt="$page.props.socialstream.prompt"
-      :labels="$page.props.socialstream.labels"
-      :providers="$page.props.socialstream.providers"
-    />
-  </AuthenticationCard>
+          <v-alert
+            density="comfortable"
+            type="info"
+            variant="tonal"
+            class="mb-4"
+          >
+            Already have an account?
+            <InertiaLink
+              :href="route('login')"
+              class="text-info"
+            >
+              Sign in
+            </InertiaLink>
+          </v-alert>
+          <p
+            class="mb-4"
+          >
+            Sign up with Email
+          </p>
+          <v-text-field
+            v-model="form.name"
+            :error-messages="form.errors.name"
+            placeholder="John Doe"
+            required
+            autofocus
+            variant="outlined"
+            hide-details="auto"
+            label="Full Name"
+            class="mb-4"
+          />
+          <v-text-field
+            v-model="form.email"
+            :error-messages="form.errors.email"
+            placeholder="john@example.com"
+            required
+            variant="outlined"
+            hide-details="auto"
+            label="Email address"
+            class="mb-4"
+          />
+          <v-text-field
+            v-model="form.password"
+            :error-messages="form.errors.password"
+            placeholder="************"
+            variant="outlined"
+            label="Password"
+            required
+            autocomplete="new-password"
+            :append-inner-icon="showP ? mdiEye : mdiEyeOff"
+            :type="showP ? 'text' : 'password'"
+            hide-details="auto"
+            class="mb-4"
+            @click:append-inner="showP = !showP"
+          />
+          <v-text-field
+            v-model="form.password_confirmation"
+            :error-messages="form.errors.password_confirmation"
+            placeholder="************"
+            variant="outlined"
+            label="Confirm Password"
+            required
+            autocomplete="new-password"
+            :append-inner-icon="showCP ? mdiEye : mdiEyeOff"
+            :type="showCP ? 'text' : 'password'"
+            hide-details="auto"
+            class="mb-4"
+            @click:append-inner="showCP = !showCP"
+          />
+
+          <div v-if="inertiaProps.jetstream?.hasTermsAndPrivacyPolicyFeature">
+            <!-- TODO checkbox slots & error messages-->
+            <v-checkbox
+              v-model="form.terms"
+              hide-details
+              :error-messages="form.errors.terms"
+              :disabled="form.processing"
+            >
+              <!-- TODO change target to _blank ????? -->
+              <template #label>
+                <span class="text-wrap">I agree to the <a
+                  target="_blank"
+                  href="terms-of-service"
+                  v-text="'Terms of Service'"
+                /> and <a
+                  target="_blank"
+                  href="privacy-policy"
+                  v-text="'Privacy Policy'"
+                />
+                </span>
+              </template>
+            </v-checkbox>
+          </div>
+
+          <div class="d-flex mb-4">
+            <v-spacer />
+            <v-btn
+              color="primary"
+              type="submit"
+              :disabled="form.processing"
+            >
+              Sign Up
+            </v-btn>
+          </div>
+        </v-form>
+        <Socialstream
+          v-if="inertiaProps.socialstream?.show && inertiaProps.socialstream.providers.length"
+          :error="inertiaProps.errors.socialstream"
+          :prompt="inertiaProps.socialstream.prompt"
+          :providers="inertiaProps.socialstream.providers"
+        />
+      </v-card-text>
+    </v-card>
+  </AuthLayout>
 </template>
