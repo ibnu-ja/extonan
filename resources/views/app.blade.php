debe9ac3e5d90209c3a4bc3176da848f89865e81
Ibnu JA <ibnu.ja@outlook.com>
2024-07-20 06:21:25 +0000

    add robot font from google

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-{{--        <link rel="preconnect" href="https://fonts.bunny.net">--}}
-{{--        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />--}}
+        <link rel="preload" as="style" onload="this.rel='stylesheet'" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
 
         <!-- Scripts -->
         @routes
