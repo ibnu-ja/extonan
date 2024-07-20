115d0b167830ba740cf29793fb06fde6d698f387
Ibnu JA <ibnu.ja@outlook.com>
2024-07-20 07:05:24 +0000

    chore: remove comments

--- resources/js/Components/ConnectedAccount.vue
+++ resources/js/Components/ConnectedAccount.vue
@@ -27,41 +27,6 @@ withDefaults(defineProps<{
       <template #append>
         <slot name="action" />
       </template>
-      <!--      <v-list-item-title>-->
-      <!--        {{ provider.name }}-->
-      <!--      </v-list-item-title>-->
-      <!--      <v-list-item-subtitle v-if="createdAt">-->
-      <!--        Conn-->
-      <!--      </v-list-item-subtitle>-->
     </v-list-item>
   </v-list>
-<!--  <div>-->
-<!--    <div class="px-3 flex items-center justify-between">-->
-<!--      <div class="flex items-center">-->
-<!--        <v-icon :icon="getIcon(provider.id)" />-->
-
-<!--        <div class="ml-2">-->
-<!--          <div class="text-sm font-semibold text-gray-600">-->
-<!--            {{ provider.name }}-->
-<!--          </div>-->
-
-<!--          <div-->
-<!--            v-if="createdAt !== null"-->
-<!--            class="text-xs text-gray-500"-->
-<!--          >-->
-<!--            Connected {{ createdAt }}-->
-<!--          </div>-->
-
-<!--          <div-->
-<!--            v-else-->
-<!--            class="text-xs text-gray-500"-->
-<!--          >-->
-<!--            Not connected.-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-
-<!--      <slot name="action" />-->
-<!--    </div>-->
-<!--  </div>-->
 </template>
