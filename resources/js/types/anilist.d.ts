a64bf343a8d3a9c4ad7cf6c28f3940c3acbabe54
Ibnu JA <ibnu.ja@outlook.com>
2024-08-04 10:10:57 +0000

    add anilist type

---
+++ resources/js/types/anilist.d.ts
@@ -0,0 +1,77 @@
+export type AnilistMedia = {
+  Media: Anime
+}
+
+export type Anime = {
+  id: number
+  episodes: number
+  coverImage: {
+    extraLarge: string
+    large: string
+    medium: string
+    color: string
+  }
+  startDate: {
+    year: number
+    month: number
+    day: number
+  }
+  endDate: {
+    year: number
+    month: number
+    day: number
+  }
+  studios: {
+    edges: {
+      node: {
+        id: number
+        name: string
+      }
+    }[]
+  }
+  characters: {
+    edges: {
+      node: {
+        id: number
+        name: {
+          full: string
+        }
+        image: {
+          large: string
+          medium: string
+        }
+      }
+      role: 'MAIN' | 'SUPPORTING' | 'BACKGROUND'
+      voiceActors: {
+        id: number
+        name: {
+          full: string
+        }
+        image: {
+          large: string
+          medium: string
+        }
+      }[]
+    }[]
+  }
+  seasonYear: number
+  // WINTER
+  // Months December to February
+
+  // SPRING
+  // Months March to May
+
+  // SUMMER
+  // Months June to August
+
+  // FALL
+  // Months September to November
+  season: 'WINTER' | 'SPRING' | 'SUMMER' | 'FALL'
+  seasonInt: number
+  genres: string[]
+  title: {
+    romaji: string
+    english: string
+    native: string
+  }
+}
