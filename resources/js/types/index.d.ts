035207efb06b0e63fab2f183e4f7fc22da75c5b6
ibnu-ja <ibnu.ja@outlook.com>
2024-08-05 18:50:55 +0000

    add paginated response

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -107,3 +107,25 @@ export type GraphQLResponse<T> = {
   data: T
   errors?: never[]
 }
+
+type Link = {
+  url: string | null
+  label: string
+  active: boolean
+}
+
+export type PaginatedResponse<T> = {
+  current_page: number
+  data: T[]
+  first_page_url: string
+  from: number
+  last_page: number
+  last_page_url: string
+  links: Link[]
+  next_page_url: string | null
+  path: string
+  per_page: number
+  prev_page_url: string | null
+  to: number
+  total: number
+}
