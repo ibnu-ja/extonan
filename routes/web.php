91afdde813e409eb06ca88f7791548abe40fff45
ibnu-ja <ibnu.ja@outlook.com>
2024-08-16 18:35:08 +0000

    homepage

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use App\Http\Controllers\AnimeController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::resources([
     'anime' => AnimeController::class,
