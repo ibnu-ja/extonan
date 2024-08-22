cd4fcc471b87c8e8dfdd49a4bf7ceffd33acc816
ibnu-ja <ibnu.ja@outlook.com>
2024-08-22 10:18:40 +0000

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.ts',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    screens: {
+      'sm': '600px',
+      'md': '960px',
+      'lg': '1280px',
+      'xl': '1920px',
+      '2xl': '2560px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
