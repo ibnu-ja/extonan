a857d80123336e7f3d20fbdaa7fb7d4001527927
Ibnu JA <ibnu.ja@outlook.com>
2024-09-01 14:44:45 +0000

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
