d3508c944267f9c9384175f00368df22dbe66cb7
ibnu-ja <ibnu.ja@outlook.com>
2026-06-08 06:27:22 +0000

    update git service check git fallback to env

--- app/Services/Git.php
+++ app/Services/Git.php
@@ -8,67 +8,35 @@ use Illuminate\Support\Facades\Process;
 
 final readonly class Git
 {
-    /**
-     * Get the current version of the site.
-     */
     public function getLatestTag(): string
     {
-        return trim(
-            Process::path(base_path())->run(['git', 'describe', '--tags', '--abbrev=0'])->output(),
-        );
+        return $this->envOrFallback('APP_VERSION', 'describe --tags --abbrev=0');
     }
 
     public function getAppBranch(): string
     {
-        // git rev-parse --abbrev-ref HEAD
-        return trim(
-            Process::path(base_path())->run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])->output(),
-        );
+        return $this->envOrFallback('APP_BRANCH', 'rev-parse --abbrev-ref HEAD');
     }
 
     public function getAppCommitHash(): string
     {
-        // git rev-parse --short HEAD
-        return trim(
-            Process::path(base_path())->run(['git', 'rev-parse', '--short', 'HEAD'])->output(),
-        );
+        return $this->envOrFallback('APP_COMMIT_HASH', 'rev-parse --short HEAD');
     }
 
     public function getRepoUrl(): ?string
     {
-        // git remote get-url origin
-        $url = trim(
-            Process::path(base_path())->run(['git', 'remote', 'get-url', 'origin'])->output()
-        );
-        if (
-            (str_contains($url, '@') && str_contains($url, ':')) &&
-            (str_contains($url, 'github.com') || str_contains($url, 'gitlab.com'))
-        ) {
-            // SSH URL for GitHub or GitLab
-            return $this->sshToHttps($url);
-        } elseif (str_starts_with($url, 'https://github.com') || str_starts_with($url, 'https://gitlab.com')) {
-            // HTTPS URL from GitHub or GitLab, return as it is
-            return $url;
-        } else {
-            // Unsupported or invalid URL format
-            return null;
-        }
+        return $this->envOrFallback('APP_REPO_URL', 'remote get-url origin');
     }
 
-    private function sshToHttps(string $sshUrl): string
+    private function envOrFallback(string $envKey, string $gitCommand): string
     {
+        $env = env($envKey);
+        if ($env !== null && $env !== '') {
+            return $env;
+        }
 
-        $parts = explode(':', $sshUrl);
-
-        // Extract username and repository name
-        $usernameAndRepo = explode('/', $parts[1]);
-        $username = $usernameAndRepo[0];
-        $repo = $usernameAndRepo[1];
-
-        // Determine the host (GitHub, GitLab, etc.)
-        $host = substr($parts[0], strpos($parts[0], '@') + 1);
-
-        // Construct the HTTPS URL
-        return "https://$host/$username/$repo";
+        return trim(
+            Process::path(base_path())->run(['git', ...explode(' ', $gitCommand)])->output(),
+        );
     }
 }
